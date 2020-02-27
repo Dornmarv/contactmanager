b765a6d1dc1ae2ce34c5b045a5273e20e8ed7126
Marvellous <user@users-MacBook-Pro.local>
2020-02-27 10:51:13 +0100

    Updates

---
+++ precache-manifest.d8fe411e262a1d4f4bfbb82197f73a73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63177dbb07a268665cb48f29fd9143eb",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "ff71b1ee17a72914bceb",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4ba800ea3985e54acdce",
+    "url": "/contactmanager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ff71b1ee17a72914bceb",
+    "url": "/contactmanager/static/js/2.4653c252.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.4653c252.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ba800ea3985e54acdce",
+    "url": "/contactmanager/static/js/main.e9b9ec5c.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
